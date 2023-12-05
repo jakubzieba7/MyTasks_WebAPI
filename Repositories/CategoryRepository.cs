8d3cd4367789dd5119f97f123e87bf6d47a0a3c8
Jakub Zięba <jakubzieba7@gmail.com>
2023-12-05 17:47:20 +0100

    Added CategoryRepository

---
+++ Repositories/CategoryRepository.cs
@@ -0,0 +1,46 @@
+﻿using MyTasks_WebAPI.Data;
+using MyTasks_WebAPI.Domains;
+
+namespace MyTasks_WebAPI.Repositories
+{
+    public class CategoryRepository
+    {
+        private ApplicationDbContext _context;
+
+        public CategoryRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Category> GetCategories(string userId)
+        {
+            return _context.Categories.Where(x => x.UserId == userId).ToList();
+        }
+
+        public Category Get(int id, string userId)
+        {
+            return _context.Categories.Single(x => x.UserId == userId && x.Id == id);
+        }
+
+        public void Add(Category category)
+        {
+            var categories = _context.Categories.Where(x => x.UserId == category.UserId);
+
+            _context.Categories.Add(category);
+        }
+
+        public void Update(Category category)
+        {
+            var categoryToUpdate = _context.Categories.Single(x => x.Id == category.Id && x.UserId == category.UserId);
+
+            categoryToUpdate.Name = category.Name;
+        }
+
+        public void Delete(int id, string userId)
+        {
+            var categoryToDelete = _context.Categories.Single(x => x.Id == id && x.UserId == userId);
+
+            _context.Categories.Remove(categoryToDelete);
+        }
+    }
+}
