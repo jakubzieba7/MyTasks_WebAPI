23933317d1ae98bf51b90683d1e1de2200643a93
Jakub Zięba <jakubzieba7@gmail.com>
2023-12-06 23:11:35 +0100

    Added DTOs and converters

---
+++ Models/Converters/CategoryConverter.cs
@@ -0,0 +1,36 @@
+﻿using MyTasks_WebAPI.Models.Domains;
+using MyTasks_WebAPI.Models.DTOs;
+
+namespace MyTasks_WebAPI.Models.Converters
+{
+    public static class CategoryConverter
+    {
+        public static CategoryDto ToDto(this Category model)
+        {
+            return new CategoryDto()
+            {
+                Id = model.Id,
+                Name = model.Name,
+                UserId = model.UserId,
+            };
+        }
+
+        public static IEnumerable<CategoryDto> ToDtos(this IEnumerable<Category> model)
+        {
+            if (model == null)
+                return Enumerable.Empty<CategoryDto>();
+
+            return model.Select(x => x.ToDto());
+        }
+
+        public static Category ToDao(this CategoryDto model)
+        {
+            return new Category()
+            {
+                Id = model.Id,
+                Name = model.Name,
+                UserId = model.UserId,
+            };
+        }
+    }
+}
---
+++ Models/Converters/TaskConverter.cs
@@ -0,0 +1,44 @@
+﻿using MyTasks_WebAPI.Models.DTOs;
+using Task = MyTasks_WebAPI.Models.Domains.Task;
+
+namespace MyTasks_WebAPI.Models.Converters
+{
+    public static class TaskConverter
+    {
+        public static TaskDto ToDto(this Task model)
+        {
+            return new TaskDto()
+            {
+                Id = model.Id,
+                Title = model.Title,
+                Description = model.Description,
+                IsExecuted = model.IsExecuted,
+                Term = model.Term,
+                CategoryId = model.CategoryId,
+                UserId = model.UserId,
+            };
+        }
+
+        public static IEnumerable<TaskDto> ToDtos(this IEnumerable<Task> model)
+        {
+            if (model == null)
+                return Enumerable.Empty<TaskDto>();
+
+            return model.Select(x => x.ToDto());
+        }
+
+        public static Task ToDao(this TaskDto model)
+        {
+            return new Task()
+            {
+                Id = model.Id,
+                Title = model.Title,
+                Description = model.Description,
+                IsExecuted = model.IsExecuted,
+                Term = model.Term,
+                CategoryId = model.CategoryId,
+                UserId = model.UserId,
+            };
+        }
+    }
+}
